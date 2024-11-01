cca761f5f62fbb4293ebb4f97beb4cc3661b92b4
李典学 <>
2024-11-01 08:53:00 +0800

    updata

--- src/app/api-interface/api.interface.ts
+++ src/app/api-interface/api.interface.ts
@@ -13,3 +13,12 @@ export interface TestInterface {
   name: string;
   getName(): string;
 }
+
+export interface ProductData {
+  category: string,
+  price: number,
+  thumbnail: string,
+  images: string[],
+  title: string,
+  id: number
+}
